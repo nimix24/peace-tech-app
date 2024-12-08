e3e7265ca15f558d8fdfb405defa3cccea5e83a3
nimix24 <89929279+nimix24@users.noreply.github.com>
2024-12-08 16:45:52 +0200

    added spaces to comments

--- flask_app.py
+++ flask_app.py
@@ -8,8 +8,8 @@ app = Flask(__name__)
 messages = []
 
 # Initialize SQS client with environment variable QUEUE_URL
-#sqs = boto3.client('sqs', region_name='us-west-2')
-#queue_url = os.getenv('QUEUE_URL')
+# sqs = boto3.client('sqs', region_name='us-west-2')
+# queue_url = os.getenv('QUEUE_URL')
 
 @app.route('/send', methods=['POST'])
 def send_message():
@@ -23,13 +23,13 @@ def send_message():
             return jsonify({'error': 'Message body is required'}), 400
 
         # Send message to SQS
-        #print(message_body)
-        #response = sqs.send_message(QueueUrl=queue_url,MessageBody=message_body)
-        #print(response)
-        #return jsonify({
+        # print(message_body)
+        # response = sqs.send_message(QueueUrl=queue_url,MessageBody=message_body)
+        # print(response)
+        # return jsonify({
          #   'MessageId': response['MessageId'],
          #   'Status': 'Message sent successfully'
-        #})
+        # })
 
         # Store the message in the in-memory list
         messages.append(message_body)
@@ -44,9 +44,9 @@ def send_message():
 def receive_message():
     try:
         # Receive message from SQS
-        #response = sqs.receive_message(QueueUrl=queue_url,MaxNumberOfMessages=10,WaitTimeSeconds=10
+        # response = sqs.receive_message(QueueUrl=queue_url,MaxNumberOfMessages=10,WaitTimeSeconds=10
             # VisibilityTimeout=5  # Set a shorter visibility timeout)
-        #messages = response.get('Messages', [])
+        # messages = response.get('Messages', [])
 
         if not messages:
             return jsonify({'Status': 'No messages available in the queue'}), 200
@@ -58,14 +58,14 @@ def receive_message():
             'Messages': received_messages,
             'Status': 'Message/s received and deleted from queue'}), 200
 
-        #message = messages[0]
-        #message_body = message['Body']
-        #receipt_handle = message['ReceiptHandle']
+        # message = messages[0]
+        # message_body = message['Body']
+        # receipt_handle = message['ReceiptHandle']
 
         # Delete the message after processing
       #  sqs.delete_message(QueueUrl=queue_url, ReceiptHandle=receipt_handle)
 
-        #return jsonify({
+        # return jsonify({
         #    'Message': message_body,
         #    'Status': 'Message received and deleted from queue',}), 200
 

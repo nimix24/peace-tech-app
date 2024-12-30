fd3662710130925c357506243851f6772f4c1bd4
nimix24 <89929279+nimix24@users.noreply.github.com>
2024-12-30 18:05:33 +0200

    Adding region = "us-west-2" to provider

--- main.tf
+++ main.tf
@@ -321,6 +321,7 @@ resource "aws_dynamodb_table" "terraform_locks" {
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "terraform-state-bucket" # Replace with a unique bucket name
   force_destroy = true # Optional: Allows destroying the bucket even if it contains objects
+  provider = aws
 
   tags = {
     Name        = "TerraformStateBucket"

47f6f1576dccfbde7917ddf09e9aa272ee33257e
nimix24 <89929279+nimix24@users.noreply.github.com>
2025-01-04 00:53:21 +0200

    create 2 tables master and ec2-branch

--- modules/dynamodb/outputs.tf
+++ modules/dynamodb/outputs.tf
@@ -6,12 +6,12 @@ output "greetings_table" {
 
 output "terraform_locks_table_ec2_branch" {
   description = "The ARN of the Terraform locks DynamoDB table"
-  #value       = aws_dynamodb_table.terraform_locks_table.arn
-  value = length(aws_dynamodb_table.terraform_locks_table_ec2_branch) > 0 ? aws_dynamodb_table.terraform_locks_table_ec2_branch[0].arn : null
+  value       = aws_dynamodb_table.terraform_locks_table_ec2_branch
+  #value = length(aws_dynamodb_table.terraform_locks_table_ec2_branch) > 0 ? aws_dynamodb_table.terraform_locks_table_ec2_branch[0].arn : null
 }
 
 output "terraform_locks_table_master" {
   description = "The ARN of the Terraform locks DynamoDB table"
-  #value       = aws_dynamodb_table.terraform_locks_table.arn
-  value = length(aws_dynamodb_table.terraform_locks_table_master) > 0 ? aws_dynamodb_table.terraform_locks_table_ec2_branch[0].arn : null
+  value       = aws_dynamodb_table.terraform_locks_table_master
+  #value = length(aws_dynamodb_table.terraform_locks_table_master) > 0 ? aws_dynamodb_table.terraform_locks_table_ec2_branch[0].arn : null
 }

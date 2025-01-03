1daad2e1fc755588a2ac8a643f74e646fbc7d462
nimix24 <89929279+nimix24@users.noreply.github.com>
2025-01-03 17:24:04 +0200

    us-west-1 tables

--- modules/dynamodb/outputs.tf
+++ modules/dynamodb/outputs.tf
@@ -1,11 +1,13 @@
 output "greetings_table_arn" {
   description = "The ARN of the greetings DynamoDB table"
   #value       = aws_dynamodb_table.greetings.arn
-  value = length(aws_dynamodb_table.greetings) > 0 ? aws_dynamodb_table.greetings[0].arn : null
+  #value = length(aws_dynamodb_table.greetings) > 0 ? aws_dynamodb_table.greetings[0].arn : null
+  value = ""
 }
 
 output "terraform_locks_table_arn" {
   description = "The ARN of the Terraform locks DynamoDB table"
   #value       = aws_dynamodb_table.terraform_locks_table.arn
-  value = length(aws_dynamodb_table.terraform_locks_table) > 0 ? aws_dynamodb_table.terraform_locks_table[0].arn : null
+  #value = length(aws_dynamodb_table.terraform_locks_table) > 0 ? aws_dynamodb_table.terraform_locks_table[0].arn : null
+  value = ""
 }
